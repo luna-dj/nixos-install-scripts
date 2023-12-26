162996566d2c4e1a10cbafa49b61f33961d34cc4
Luna <57094482+luna-dj@users.noreply.github.com>
2023-12-26 14:44:55 +0100

    Update nixos-install-hetzner-cloud.sh

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAl9nUQY9/GLwwCIkmsbMNLcdpx4wzFFmmTklbS9DP6D eddsa-key-20231218"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
